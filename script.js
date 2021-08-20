3f36fb9c98b60933bb0d9b7ce8929ca12f4a5544
natasaghinadze <85290787+natasaghinadze@users.noreply.github.com>
2021-08-20 09:11:42 -0700

    edit

--- script.js
+++
@@ -1,9 +0,0 @@
-// var burg = document.querySelector(".fa-bars")
-// burg.addEventListener("click",function(){
-//     var z = document.querySelector(".naviganionnn") 
-//     if(z.style.display==="block"){
-//         z.style.display="none"
-        
-//     }
-//     else{z.style.display="block"}
-// })

bd911522095371464006d99cfbf52c68d47360d6
natasaghinadze <85290787+natasaghinadze@users.noreply.github.com>
2021-08-20 06:34:30 -0700

    add js

---
+++ script.js
@@ -0,0 +1,9 @@
+var burg = document.querySelector(".fa-bars")
+burg.addEventListener("click",function(){
+    var z = document.querySelector(".naviganionnn") 
+    if(z.style.display==="block"){
+        z.style.display="none"
+        
+    }
+    else{z.style.display="block"}
+})
